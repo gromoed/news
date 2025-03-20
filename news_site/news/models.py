5aa15d6ba90bd839b3f2594e2960999f650721e7
gromoed <sasha2004334@gmail.com>
2025-03-21 00:33:03 +0600

    add category form

--- news_site/news/models.py
+++ news_site/news/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    
+    def __str__(self):
+        return self.name
+
+
 class NewsArticle(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.CharField(max_length=50)
+    category = models.ManyToManyField(Category, related_name="articles", default=None)
 
     def __str__(self):
         return self.title
-    
-
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-    
-    def __str__(self):
-        return self.name
