16bd2bb6647ac1a153ec498947b1b23fcd4013da
gromoed <sasha2004334@gmail.com>
2025-03-20 23:51:29 +0600

    Created model for category

--- news_site/news/models.py
+++ news_site/news/models.py
@@ -9,3 +9,10 @@ class NewsArticle(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
